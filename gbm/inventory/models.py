5cb2adddf458d1e5269a96503101d6f2e8f774ea
Benjamin Buckmaster <benjaminbuckmaster@icloud.com>
2024-02-12 13:48:24 +1100

    added model Bean

--- gbm/inventory/models.py
+++ gbm/inventory/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Bean(models.Model):
+    name = models.CharField(max_length=50)
+    origin = models.CharField(max_length=50, blank=True)
+    supplier = models.CharField(max_length=50, blank=True)
+    notes = models.CharField(max_length=500, blank=True)
+    reorder_trigger = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    reorder_qty = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+
+    # override string representation for developing purposes
+    def __str__(self) -> str:
+        return f"{self.name}"
