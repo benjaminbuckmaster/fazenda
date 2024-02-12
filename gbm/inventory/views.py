c08d424a3f11ed32bb7f9e6bf8b0db0f83f2a8fc
Benjamin Buckmaster <benjaminbuckmaster@icloud.com>
2024-02-12 14:19:24 +1100

    connected bean information to html

--- gbm/inventory/views.py
+++ gbm/inventory/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Bean
 
 def home(request):
-    return render(request, 'home.html', {})
+    # get the bean information from the database
+    beans = Bean.objects.all()
+
+    # show the homepage and pass through bean information
+    return render(request, 'home.html', {'beans':beans})
