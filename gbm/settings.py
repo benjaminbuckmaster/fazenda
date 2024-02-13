ba1485cb5a6faf41da6fb75c048036d742fa3169
Benjamin Buckmaster <benjaminbuckmaster@icloud.com>
2024-02-13 15:11:15 +1100

    removed environ

--- gbm/settings.py
+++ gbm/settings.py
@@ -13,14 +13,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
-# import environ
-
-# env = environ.Env()
-# environ.Env.read_env()
-
-# # Django environ secret key
-# SECRET_KEY = env("SECRET_KEY")
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
