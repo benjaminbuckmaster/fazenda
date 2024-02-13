e34f9ae009a4f10f767bd1d52139bc42d3988795
Benjamin Buckmaster <benjaminbuckmaster@icloud.com>
2024-02-13 17:07:41 +1100

    added StockEntry model

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Bean
+from .models import Bean, StockEntry
 
 admin.site.register(Bean)
+admin.site.register(StockEntry)
--- inventory/models.py
+++ inventory/models.py
@@ -11,3 +11,17 @@ class Bean(models.Model):
     # override string representation for developing purposes
     def __str__(self) -> str:
         return f"{self.name}"
+
+class StockEntry(models.Model):
+    date = models.DateField(auto_now=False, auto_now_add=True) # auto_now_add set to true for creation date
+    bean = models.ForeignKey("Bean", on_delete=models.CASCADE)
+    qty_added = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    qty_used = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    qty_total = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.date} {self.bean.name} ({self.qty_added}, {self.qty_used}, {self.qty_total})"
+    
+    class Meta:
+        verbose_name = "Stock Entry"
+        verbose_name_plural = "Stock Entries"
