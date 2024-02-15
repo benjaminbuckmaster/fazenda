f4720f47fc1b8c98edca7c00a09ebba25aafcd26
Benjamin Buckmaster <benjaminbuckmaster@icloud.com>
2024-02-15 14:30:59 +1100

    added navbar with logo and buttons

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('bean-information/', views.bean_information, name='bean-information'),
+    path('stock-management/', views.stock_management, name='stock-management'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -7,3 +7,9 @@ def home(request):
 
     # show the homepage and pass through bean information
     return render(request, 'home.html', {'beans':beans})
+
+def stock_management(request):
+    pass
+
+def bean_information(request):
+    pass
