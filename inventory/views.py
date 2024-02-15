6dc293320f7641bfd0cee7d9f6c954aff3d2b785
Benjamin Buckmaster <benjaminbuckmaster@icloud.com>
2024-02-15 15:37:32 +1100

    moved bean information to its own page

--- inventory/views.py
+++ inventory/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 from .models import Bean
 
 def home(request):
-    # get the bean information from the database
-    beans = Bean.objects.all()
-
-    # show the homepage and pass through bean information
-    return render(request, 'home.html', {'beans':beans})
+    # show the homepage
+    return render(request, 'home.html', {})
 
 def stock_management(request):
     pass
 
 def bean_information(request):
-    pass
+    # get the bean information from the database
+    beans = Bean.objects.all()
+
+    # shows bean information and passes through bean information
+    return render(request, 'bean.html', {'beans':beans})
