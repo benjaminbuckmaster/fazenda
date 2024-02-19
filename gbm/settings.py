235a7ec4f6d0830bbb17918b3379e0d33829cb4b
Benjamin Buckmaster <benjaminbuckmaster@icloud.com>
2024-02-19 15:14:14 +1100

    setup for dev depployment

--- gbm/settings.py
+++ gbm/settings.py
@@ -88,9 +88,19 @@ WSGI_APPLICATION = 'gbm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
+# deployment
 DATABASES = {
-    'default': env.db()
+    'default': env.db(),
 }
+
+# development
+# DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.sqlite3",
+#             "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#         }
+#     }
 
 
 # Password validation
